46153688af8ec000d0853bc6a2edd6075a0317f1
Sanjay Josh <Sanjay.Josh@github.com>
2018-04-09 04:45:43 +0000

    Errors

--- debb.py
+++ debb.py
@@ -5,10 +5,10 @@ import time
 import os.path
 from utils import mac_remove_file
 import tensorflow as tf
+import sys
 from feature_extractor import graph
 def train():
     with graph.as_default():
-        is_multiprocessing=True
         mac_remove_file()
         starttime=time.time()
         datamodel = Dataset(False)
@@ -23,22 +23,22 @@ def train():
         early_stopper = EarlyStopping(patience=15)
         feature_dim= 2048
         sampling_rate=40
-        batchsize=40
-        epochs= 1000
+        batchsize=6
+        epochs= 100
         steps_per_epoch= datamodel.trainlength//batchsize
         steps_per_epoch_test = datamodel.testlength//batchsize
         dnn_model = lstm_model(nb_categories,sampling_rate,feature_dim)
-        model= dnn_model.getmodel()
-        train_generator= datamodel.train_data_generator(batchsize)
-        test_generator= datamodel.test_data_generator(batchsize)
+        model= dnn_model.getmodel();sys.exit(0)
+        # train_generator= datamodel.train_data_generator(batchsize)
+        # test_generator= datamodel.test_data_generator( batchsize)
         starttime=time.time()
-        model.fit_generator(generator=train_generator,steps_per_epoch=steps_per_epoch,epochs=epochs,verbose=1,validation_data=test_generator,workers=4,validation_steps=steps_per_epoch_test,callbacks=[tb,checkpoint,early_stopper])
-        #X,y = datamodel.load_all_in_memory(datamodel.trainlist)
-        #X_test,y_test = datamodel.load_all_in_memory(datamodel.testlist)
-        #print("Time for loading into memory is:",time.time() - starttime)
+        # model.fit_generator(generator=train_generator,steps_per_epoch=steps_per_epoch,epochs=epochs,verbose=1,validation_data=test_generator,validation_steps=steps_per_epoch_test,callbacks=[tb])
+        X,y = datamodel.load_all_in_memory(datamodel.trainlist)
+        X_test,y_test = datamodel.load_all_in_memory(datamodel.testlist)
+        print("Time for loading into memory is:",time.time() - starttime)
 
-        #starttime=time.time()
-        #model.fit(X,y,validation_data=(X_test, y_test),batch_size=batchsize,verbose=1,epochs=epochs,callbacks=[tb,checkpoint,early_stopper])
+        starttime=time.time()
+        model.fit(X,y,validation_data=(X_test, y_test),batch_size=batchsize,verbose=1,epochs=epochs,callbacks=[tb,checkpoint,early_stopper])
         print("Training-Validation time is",time.time() - starttime)
         # X,y = datamodel.data_generator(datamodel.trainlist,batchsize)
         # print(X.shape)
