d3a454a298b058c5fe67c37548cd61ae12758ba8
s0j00os <Sanjay.Josh@github.com>
2018-04-10 02:38:25 +0530

    Non multi

--- datagen.py
+++ datagen.py
@@ -1,48 +1,163 @@
-from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, CSVLogger
-from lstm_model import lstm_model
-from datagen import Dataset
-import time
-import os.path
+from create_images import extract_images, sample_x_images,destination_directory
+import sys
+from split_list import make_split,getclasses,category_to_digit,train_folder,test_folder,digit_to_category
+from feature_extractor import Inception_Features, Resnet_Features
+from keras.utils import to_categorical
 from utils import mac_remove_file
+import csv
+import numpy as np
+import threading
+import random
 import tensorflow as tf
-from feature_extractor import graph
-def train():
-    with graph.as_default():
-        is_multiprocessing=True
-        mac_remove_file()
-        starttime=time.time()
-        datamodel = Dataset(False)
-        print("Done with the file-creation")
-        datamodel.make_path_lists()
-        datamodel.save_in_disk(datamodel.trainlist)
-        datamodel.save_in_disk(datamodel.testlist)
-        print("Done with the path-creation")
-        print("Time for path-creation is:",time.time() - starttime)
-        nb_categories=datamodel.class_num
-        print(nb_categories)
-        tb = TensorBoard(log_dir=os.path.join('logs'))
-        checkpoint= ModelCheckpoint(filepath=os.path.join('checkpoints','lstm-'+'.{epoch:03d}-{val_loss:.3f}.hdf5'),verbose=1,save_best_only=True)
-        early_stopper = EarlyStopping(patience=15)
-        feature_dim= 2048
-        sampling_rate=30
-        batchsize=40
-        epochs= 1000
-        steps_per_epoch= datamodel.trainlength//batchsize
-        steps_per_epoch_test = datamodel.testlength//batchsize
-        dnn_model = lstm_model(nb_categories,sampling_rate,feature_dim)
-        model= dnn_model.getmodel()
-        #train_generator= datamodel.train_data_generator(batchsize)
-        #test_generator= datamodel.test_data_generator(batchsize)
-        starttime=time.time()
-        X_test,y_test = datamodel.load_all_in_memory(datamodel.testlist)
-        #model.fit_generator(generator=train_generator,steps_per_epoch=steps_per_epoch,epochs=epochs,verbose=1,validation_data=(X_test, y_test),workers=4,validation_steps=steps_per_epoch_test,callbacks=[tb,checkpoint,early_stopper])
-        #model.fit_generator(generator=train_generator,steps_per_epoch=steps_per_epoch,epochs=epochs,verbose=1,validation_data=(X_test, y_test),workers=4,callbacks=[tb,checkpoint,early_stopper])
-        #X,y = datamodel.load_all_in_memory(datamodel.trainlist)
-        #print("Time for loading into memory is:",time.time() - starttime)
+import sys
+import os
+import multiprocessing as mp
+class threadsafe_iterator:
+    def __init__(self, iterator):
+        self.iterator = iterator
+        self.lock = threading.Lock()
 
-        #starttime=time.time()
-        #model.fit(X,y,validation_data=(X_test, y_test),batch_size=batchsize,verbose=1,epochs=epochs,callbacks=[tb,checkpoint,early_stopper])
-        print("Training-Validation time is",time.time() - starttime)
-        # X,y = datamodel.data_generator(datamodel.trainlist,batchsize)
-        # print(X.shape)
-        # print(y.shape)
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with self.lock:
+            return next(self.iterator)
+
+def threadsafe_generator(func):
+    def gen(*a, **kw):
+        return threadsafe_iterator(func(*a, **kw))
+    return gen
+
+class Dataset():
+    all_classes=None
+    class_dict=None
+    sampling_rate=40
+    trainlength= None
+    testlength= None
+    trainfile="trainlist-00.csv"
+    testfile="testlist-00.csv"
+    trainlist=None
+    testlist=None
+    def set_sampling_rate(rate):
+        self.sampling_rate=rate
+    def __init__(self,isinit=False,transfer="I"):
+        if isinit == True:
+            self.trainfile,self.testfile=make_split(80,20,"00")
+            extract_images(self.trainfile,self.testfile)
+
+
+        self.all_classes = getclasses()
+        self.class_num = len(self.all_classes)
+        self.class_dict = digit_to_category()
+        if transfer == "I":
+            self.feature_class = Inception_Features()
+        else:
+            self.feature_class = Resnet_Features()
+
+    def make_path_lists(self):
+        self.trainlist=list()
+        with open(self.trainfile,"r") as file:
+            csvreader = csv.reader(file)
+            for row in csvreader:
+                videoname,dest_dir=destination_directory(row,train_folder)
+                self.trainlist.append([dest_dir,int(row[1])])
+        self.trainlength=len(self.trainlist)
+        self.testlist=list()
+        with open(self.testfile,"r") as file:
+            csvreader = csv.reader(file)
+            for row in csvreader:
+                videoname,dest_dir=destination_directory(row,test_folder)
+                self.testlist.append([dest_dir,int(row[1])])
+        self.testlength=len(self.testlist)
+    @threadsafe_generator
+    def train_data_generator(self,batchsize):
+        imagelist=self.trainlist
+        j=0
+        while True:
+            X=[]
+            y=[]
+            for i in range(batchsize):
+                j=j+1
+                if j == self.trainlength :
+                    j=0
+                #filename = random.choice(imagelist)
+                filename = imagelist[j]
+                # print("Rofl:",filename)
+                all_images = sample_x_images(filename[0],self.sampling_rate)
+                sequence=self.build_sequence(all_images)
+                X.append(sequence)
+                y.append(to_categorical(filename[1],self.class_num).squeeze())
+            yield np.array(X),np.array(y)
+    @threadsafe_generator
+    def test_data_generator(self,batchsize):
+        imagelist=self.testlist
+        j=0
+        while True:
+            X=[]
+            y=[]
+            for i in range(batchsize):
+                j=j+1
+                if j == self.testlength :
+                    j=0
+                #filename = random.choice(imagelist)
+                filename = imagelist[j]
+                # print("Rofl:",filename)
+                all_images = sample_x_images(filename[0],self.sampling_rate)
+                sequence=self.build_sequence(all_images)
+                X.append(sequence)
+                y.append(to_categorical(filename[1],self.class_num).squeeze())
+            yield np.array(X),np.array(y)
+    def load_all_in_memory(self,listname):
+        X=[]
+        y=[]
+        for each_file in listname:
+            imagelist = sample_x_images(each_file[0],self.sampling_rate)
+            sequence=self.build_sequence(imagelist)
+            X.append(sequence)
+            y.append(to_categorical(each_file[1],self.class_num).squeeze())
+        return np.array(X), np.array(y)
+    def save_array(each_file):
+        imagelist = sample_x_images(each_file[0],self.sampling_rate)
+        X=self.build_sequence(imagelist)
+        y=to_categorical(each_file[1],self.class_num).squeeze()
+        np.save(os.path.join(each_file[0],'X'),X)
+        np.save(os.path.join(each_file[0],'y'),y)
+        return 1;
+
+    def save_in_disk_parallel(self,listname):
+        pool = mp.Pool(processes=8)
+        results = pool.map(save_array,listname)
+        # imagelist = sample_x_images(each_file[0],self.sampling_rate)
+        # sequence=self.build_sequence(imagelist)
+        # X=sequence
+        # y=to_categorical(each_file[1],self.class_num).squeeze()
+        # np.save(os.path.join(each_file[0],'X'),X)
+        # np.save(os.path.join(each_file[0],'y'),y)
+    def save_in_disk(self,listname):
+          for each_file in listname:
+              imagelist = sample_x_images(each_file[0],self.sampling_rate)
+              sequence=self.build_sequence(imagelist)
+              X=sequence
+              y=to_categorical(each_file[1],self.class_num).squeeze()
+              np.save(os.path.join(each_file[0],'X.npy'),X)
+              np.save(os.path.join(each_file[0],'y.npy'),y)
+
+    def build_sequence(self,imagelist):
+        sequence=[self.feature_class.get_features(i) for i in imagelist]
+        # print(sequence.shape)
+        # sys.exit(0)
+        return sequence
+
+
+if __name__ == "__main__":
+    mac_remove_file()
+    length = len(sys.argv)
+    if length>2 and sys.argv[1] == "init":
+        myclass = Dataset(True)
+    else:
+        myclass = Dataset()
+        myclass.make_path_lists()
+        myclass.load_all_in_memory(myclass.trainlist)
+        print(myclass.all_classes)
+        print(myclass.class_dict)
