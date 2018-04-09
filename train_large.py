65442e13cc778e941ef2aa2c906995d8515eb343
s0j00os <Sanjay.Josh@github.com>
2018-04-10 02:11:33 +0530

    dsfd

--- train_large.py
+++ train_large.py
@@ -10,11 +10,13 @@ import multiprocessing as mp
 import numpy as np
 from feature_extractor import graph
 from feature_extractor import Inception_Features
+from image_processor import inception_image_processor
+from keras.models import Model, load_model
 from keras.utils import to_categorical
 def global_save_array(each_file):
     print("Woah here eh")
     imagelist = sample_x_images(each_file[0],datamodel.sampling_rate)
-    X=[feature_class.get_features(i) for i in imagelist]
+    X=[feature_model.predict(inception_image_processor(i,input_shape)) for i in imagelist]
     y=to_categorical(each_file[1],datamodel.class_num).squeeze()
     np.save(os.path.join(each_file[0],'X'),X)
     np.save(os.path.join(each_file[0],'y'),y)
@@ -33,9 +35,14 @@ def train():
         mac_remove_file()
         starttime=time.time()
         global datamodel
-        global feature_class
+        global feature_model
+        global input_shape=(299,299,3)
         datamodel = Dataset(False)
-        feature_class = Inception_Features()
+        inception_model = InceptionV3(weights='imagenet',include_top=True)
+        feature_model = Model(
+            inputs=inception_model.input,
+            outputs=inception_model.layers[-2].output
+        )
         print("Done with the file-creation")
         datamodel.make_path_lists()
         global_save_in_disk_parallel(datamodel.trainlist)
