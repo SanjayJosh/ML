aa9b8eb4ff23d9e4247a86657901a8de7db27834
s0j00os <Sanjay.Josh@github.com>
2018-04-10 10:54:08 +0530

    Non multi

--- train_large.py
+++ train_large.py
@@ -10,11 +10,19 @@ import multiprocessing as mp
 import numpy as np
 from feature_extractor import graph
 from feature_extractor import Inception_Features
-from keras.applications.inception_v3 import InceptionV3
 from image_processor import inception_image_processor
-from keras.models import Model, load_model
-from keras.utils import to_categorical
+
 def global_save_array(each_file):
+    from keras.applications.inception_v3 import InceptionV3
+    from keras.models import Model, load_model
+    from keras.utils import to_categorical
+    input_shape=(299,299,3)
+    inception_model = InceptionV3(weights='imagenet',include_top=True)
+    feature_model = Model(
+        inputs=inception_model.input,
+        outputs=inception_model.layers[-2].output
+    )
+    feature_model._make_predict_function()
     print("Woah here eh")
     imagelist = sample_x_images(each_file[0],datamodel.sampling_rate)
     X=[feature_model.predict(inception_image_processor(i,input_shape)) for i in imagelist]
@@ -36,8 +44,8 @@ def train():
         mac_remove_file()
         starttime=time.time()
         global datamodel
-        global feature_model
-        global input_shape
+        #global feature_model
+        #global input_shape
         datamodel = Dataset(False)
         input_shape=(299,299,3)
         inception_model = InceptionV3(weights='imagenet',include_top=True)
@@ -45,7 +53,6 @@ def train():
             inputs=inception_model.input,
             outputs=inception_model.layers[-2].output
         )
-        feature_model._make_predict_function()
         print("Done with the file-creation")
         datamodel.make_path_lists()
         global_save_in_disk_parallel(datamodel.trainlist)
